4ffbe9c0d70ce588c095e01fe5acbc12a90a4f2d
Michael Lopez <michael.94lopez@gmail.com>
2022-07-29 22:06:39 -0500

    passed first two tests on users/register

--- api/users.js
+++ api/users.js
@@ -2,45 +2,59 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const router = express.Router();
-const JWT_SECRET = process.env
+const { JWT_SECRET } = process.env;
 
 const { getUserByUsername, createUser } = require("../db");
 
 // POST /api/users/register
 router.post("/register", async (req, res, next) => {
-    console.log("this is a console log")
   const { username, password } = req.body;
 
   try {
     const _user = await getUserByUsername(username);
 
     if (_user) {
-        res.status
+      res.status(401);
       next({
         name: "UserExistsError",
         message: "A user by that username already exists.",
       });
-    }
+    } else if (password.length < 8) {
+      res.status(401);
+      next({
+        name: "PasswordTooShortError",
+        message: "Password is Too Short!",
+      });
+    } else {
+      const user = await createUser({
+        username,
+        password,
+      });
 
-    const user = await createUser({
-      username,
-      password,
-    });
-    const token = jwt.sign(
-      {
-        id: user.id,
-        username: user.username,
-      },
-      JWT_SECRET,
-      {
-        expiresIn: "1w",
+      if (!user) {
+        req.status(400);
+        next({
+          name: "UserCreationError",
+          message: "Bad Request",
+        });
+      } else {
+        const token = jwt.sign(
+          {
+            id: user.id,
+            username: user.username,
+          },
+          JWT_SECRET,
+          {
+            expiresIn: "1w",
+          }
+        );
+        res.send({
+          user,
+          message: "thank you for signing up",
+          token,
+        });
       }
-    );
-    res.send({
-        user,
-      message: "thank you for signing up",
-      token,
-    });
+    }
   } catch ({ name, message }) {
     next({ name, message });
   }
